fd12552fd90852b2dd6dbb29b779b166fe64374b
Subash <Subashtech14@gmail.com>
2023-04-17 18:59:33 +0530

    Dictionaries

---
+++ HashTableandHashMap.py
@@ -0,0 +1,9 @@
+#Hash table or a hashmap is a type of data structure that maps keys to its value pairs.
+#Creating Dictionaries
+#Using Curly Braces
+my_dict={'subash':'001','devil':'002','God':'005'}
+print(my_dict)
+print(type(my_dict))
+#Using dict()
+new_dict=dict(Dave='001',Subash='002')
+print(new_dict)
