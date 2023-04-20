31a0b9716284dcf0f662211e97c8885660999b39
Subash <Subashtech14@gmail.com>
2023-04-20 11:13:34 +0530

    Decorators Finished

--- Decorators/Singletonclass.py
+++ Decorators/Singletonclass.py
@@ -15,4 +15,17 @@ first=one()
 second=one()
 print(first is second)
 
-        
+#Nested Decorators
+def repeat(num):
+    def decorator_repeat(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            for _ in range(num):
+                value=func(*args, **kwargs)
+            return value
+        return wrapper
+    return decorator_repeat
+@repeat(num=5)
+def function(name):
+    print(f"{name}")
+function("Python")
