9123e58e33dcb9729d9b44be09fb78c57e5fc449
Subash <Subashtech14@gmail.com>
2023-04-26 22:29:21 +0530

    Modules Finishe

---
+++ Module/Module.py
@@ -0,0 +1,9 @@
+#A Module is simply a file contaning python code,It may contain function, classes etc..
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def prod(a,b):
+    return a*b
+def div(a,b):
+    return a/b
---
+++ Module/deemo.py
@@ -0,0 +1,2 @@
+from Module import *
+print(add(20, 40))
